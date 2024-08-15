0bdaebe8d496e8982e415aa8f0d3367309479b71
Emad Lashkari <emadlashkari.82@gmail.com>
2024-08-15 15:31:15 +0330

    create foot analize section

---
+++ components/sections/FootAnalise.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FootAnalise = () => {
+  return (
+    <div className="w-full h-[100vh]">
+      <h2 className="text-black">
+        Experience the <span className="text-primary950">Ultimate</span> in
+        Insole Technology
+      </h2>
+      {/* <Image  /> */}
+    </div>
+  );
+};
+
+export default FootAnalise;
