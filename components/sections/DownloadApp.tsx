5484412cf674309ee49ce1feaea2a635f43bb39b
Emad Lashkari <emadlashkari.82@gmail.com>
2024-08-15 15:31:29 +0330

    create download app section

---
+++ components/sections/DownloadApp.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const DownloadApp = () => {
+  return (
+    <div>
+      <Discription />
+    </div>
+  );
+};
+
+export default DownloadApp;
+
+function Discription() {
+  return (
+    <div>
+      <h2 className="font-poppins font-bold text-black-bold">
+        Experience the Insole Technology
+      </h2>
+      <p>
+        Welcome to our insole ordering application. We are committed to
+        protecting your privacy and ensuring the security of your personal
+        information.
+      </p>
+    </div>
+  );
+}
