6b445b7375c5aeeb088fa374c9906eccc91a69c4
Emad Lashkari <emadlashkari.82@gmail.com>
2024-08-16 22:35:41 +0330

    comment data

---
+++ data/comments/comments.ts
@@ -0,0 +1,28 @@
+import ali from "@/public/image/ali.svg";
+import maryam from "@/public/image/maryam.svg";
+import saeed from "@/public/image/saeed.svg";
+
+export const data: {
+  title: string;
+  name: string;
+  url: any;
+}[] = [
+  {
+    title:
+      "These customized insoles reduced my foot pain significantly. They're expertly crafted under medical supervision.",
+    name: "Ali Rezaei",
+    url: ali,
+  },
+  {
+    title:
+      "These customized insoles reduced my foot pain significantly. They're expertly crafted under medical supervision.",
+    name: "Ali Rezaei",
+    url: maryam,
+  },
+  {
+    title:
+      "These customized insoles reduced my foot pain significantly. They're expertly crafted under medical supervision.",
+    name: "Ali Rezaei",
+    url: saeed,
+  },
+];
