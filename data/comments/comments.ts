5579bb941b888e903bac3b5870973d4d7529e42b
Emad Lashkari <emadlashkari.82@gmail.com>
2024-08-23 00:12:51 +0330

    add more comments

--- data/comments/comments.ts
+++ data/comments/comments.ts
@@ -25,4 +25,22 @@ export const data: {
     name: "Ali Rezaei",
     url: saeed,
   },
+  {
+    title:
+      "These customized insoles reduced my foot pain significantly. They're expertly crafted under medical supervision.",
+    name: "Ali Rezaei",
+    url: ali,
+  },
+  {
+    title:
+      "These customized insoles reduced my foot pain significantly. They're expertly crafted under medical supervision.",
+    name: "Ali Rezaei",
+    url: maryam,
+  },
+  {
+    title:
+      "These customized insoles reduced my foot pain significantly. They're expertly crafted under medical supervision.",
+    name: "Ali Rezaei",
+    url: saeed,
+  },
 ];
