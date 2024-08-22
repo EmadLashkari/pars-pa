92a07c51399471b0366126cc41c350f527f84874
EmadLashkari <66029542+EmadLashkari@users.noreply.github.com>
2024-08-23 00:20:36 +0330

    Update OpinionAnime.tsx

--- components/animations/OpinionAnime.tsx
+++ components/animations/OpinionAnime.tsx
@@ -3,7 +3,7 @@
 import React, { ReactNode, useState } from "react";
 import { motion } from "framer-motion";
 
-const OpinionAnime = ({ children, data }: { children: ReactNode }) => {
+const OpinionAnime = ({ children }: { children: ReactNode }) => {
   return (
     <motion.div className="flex  flex-col justify-center items-center relative"></motion.div>
   );
